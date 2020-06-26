669b79635e0ee30a24dab9baef72f82fce3b378f
Harry Day <harry@harryday.xyz>
2020-06-26 18:19:18 +0100

    Initial Commit

---
+++ SongLinkr/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SongLinkr
+//
+//  Created by Harry Day on 26/06/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SongLinkr/SongLinkrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SongLinkrApp.swift
+//  SongLinkr
+//
+//  Created by Harry Day on 26/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct SongLinkrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
