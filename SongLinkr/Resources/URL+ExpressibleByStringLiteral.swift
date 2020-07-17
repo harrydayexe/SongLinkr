2b0f5d32d011a55cdfa9d632b1693d9c545bea03
Harry Day <harry@harryday.xyz>
2020-07-17 08:58:34 +0100

    Added URL from string literal and require functions

---
+++ SongLinkr/Resources/URL+ExpressibleByStringLiteral.swift
@@ -0,0 +1,15 @@
+//
+//  URL+ExpressibleByStringLiteral.swift
+//  SongLinkr
+//
+//  Created by Harry Day on 17/07/2020.
+//
+
+import Foundation
+
+extension URL: ExpressibleByStringLiteral {
+    // By using 'StaticString' we disable string interpolation, for safety
+    public init(stringLiteral value: StaticString) {
+        self = URL(string: "\(value)").require(hint: "Invalid URL string literal: \(value)")
+    }
+}
