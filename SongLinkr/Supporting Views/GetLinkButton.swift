5f4121e445ce6989fee81292ac4f221bb461db89
Harry Day <harry@harryday.xyz>
2020-07-19 10:13:48 +0100

    Working auto open feature

--- SongLinkr/Supporting Views/GetLinkButton.swift
+++ SongLinkr/Supporting Views/GetLinkButton.swift
@@ -5,6 +5,7 @@
 //  Created by Harry Day on 06/07/2020.
 //
 
+import Foundation
 import SwiftUI
 
 struct GetLinkButton: View {
@@ -14,6 +15,7 @@ struct GetLinkButton: View {
     @Binding var response: [PlatformLinks]
     @State var showError: Bool = false
     @State var errorDescription: (String, String) = ("","")
+    @EnvironmentObject var userSettings: UserSettings
     
     var body: some View {
         Button(action: {
@@ -27,6 +29,16 @@ struct GetLinkButton: View {
                                 let decodedResponse = try JSONDecoder().decode(SongLinkAPIResponse.self, from: data)
                                 self.response = Network.fixDictionaries(response: decodedResponse).sorted(by: { $0.id.rawValue < $1.id.rawValue })
                                 print(self.response)
+                                
+//                                Auto Open if turned on
+                                if userSettings.autoOpen {
+                                    if let defaultPlatformIndex = response.firstIndex(where: { $0.id == userSettings.defaultPlatform }) {
+                                        let defaultPlatform = response[defaultPlatformIndex]
+                                        DispatchQueue.main.async {
+                                            UIApplication.shared.open(defaultPlatform.nativeAppUriMobile ?? defaultPlatform.url)
+                                        }
+                                    }
+                                }
                                 
                                 self.callInProgress = false
                                 self.showResults = true
@@ -63,5 +75,6 @@ struct GetLinkButton: View {
 struct GetLinkButton_Previews: PreviewProvider {
     static var previews: some View {
         GetLinkButton(callInProgress: .mock(false), searchURL: .constant("https://youtu.be/9XvXLrWgA"), showResults: .mock(false), response: .constant([]))
+            .environmentObject(UserSettings())
     }
 }
