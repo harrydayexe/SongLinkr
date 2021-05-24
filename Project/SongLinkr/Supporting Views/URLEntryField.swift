41ce661702832711fefce610fe2ea0a5c43e053f
Harry Day <contact@harryday.xyz>
2021-05-25 00:10:14 +0100

    Improved Search Icon Accessibility at high font sizes Added Spanish Translations

---
+++ Project/SongLinkr/Supporting Views/URLEntryField.swift
@@ -0,0 +1,60 @@
+//
+//  URLEntryField.swift
+//  SongLinkr
+//
+//  Created by Harry Day on 24/05/2021
+//  
+//
+//  Twitter: https://twitter.com/realharryday
+//  Github: https://github.com/harryday123
+//
+
+
+import SwiftUI
+
+struct URLEntryField: View {
+    /// The binding to the property where the user input is stored
+    @Binding var searchURL: String
+    
+    /// Determines if the clear button should be shown
+    @State private var isEditing: Bool = false
+    
+    var body: some View {
+        HStack {
+            // Search Icon
+            Image(systemName: "magnifyingglass")
+                .font(Font.body.weight(.semibold))
+                .foregroundColor(.gray)
+                .accessibility(hidden: true)
+            
+            // Search Field
+            TextField("Paste a URL to match on other platforms", text: self.$searchURL)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(maxWidth: 700)
+                .keyboardType(.URL)
+                .textContentType(.URL)
+                .onTapGesture {
+                    self.isEditing = true
+                }
+                .accessibility(addTraits: .isSearchField)
+                .accessibility(label: Text("URL Search Field"))
+                .accessibility(hint: Text("Paste a URL to search for matches on other platforms."))
+            
+            if isEditing {
+                Button(action: {
+                    self.searchURL = ""
+                }) {
+                    Image(systemName: "multiply.circle.fill")
+                        .foregroundColor(.gray)
+                }
+                .accessibility(label: Text("Clear Search Bar"))
+            }
+        }
+    }
+}
+
+struct URLEntryFieldPreview: PreviewProvider {
+    static var previews: some View {
+        URLEntryField(searchURL: .constant(""))
+    }
+}
